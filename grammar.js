fc674abd5055e8e7b5e65ab19534ce30bf32b6c6
Simon Johnston <johnstonskj@gmail.com>
2025-03-21 11:45:02 -0700

    build: version bumps

--- grammar.js
+++ grammar.js
@@ -2,7 +2,7 @@
 //
 // Project:    tree-sitter-sdml
 // Author:     Simon Johnston <johntonskj@gmail.com>
-// Version:    0.4.5
+// Version:    0.4.6
 // Repository: https://github.com/johnstonskj/tree-sitter-sdml
 // License:    Apache 2.0 (see LICENSE file)
 // Copyright:  Copyright (c) 2023 Simon Johnston

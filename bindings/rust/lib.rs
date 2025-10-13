a95624c626cf2dfeafdbb25ff1c6d852242ec595
Simon Johnston <johnstonskj@gmail.com>
2025-10-13 13:14:48 -0700

    feat: use tree-sitter-language for api

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(&tree_sitter_sdml::language()).expect("Error loading sdml grammar");
+//! parser.set_language(&tree_sitter_sdml::LANGUAGE.into()).expect("Error loading sdml grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -15,18 +15,16 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_sdml() -> Language;
+    fn tree_sitter_sdml() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
+/// The tree-sitter [`LanguageFn`][LanguageFn] for this grammar.
 ///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_sdml() }
-}
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_sdml) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -35,23 +33,31 @@ pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 pub const GRAMMAR_VERSION: &str = env!("CARGO_PKG_VERSION");
 
-// Uncomment these to include any queries that this grammar contains
-
+/// The syntax highlighting query for this language.
 pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+
+/// The injections query for this language.
 pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+
+/// The locals query for this language.
 pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
+
+/// The symbol tagging query for this language.
 pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
 #[cfg(test)]
 mod tests {
     use std::fs;
 
+    pub fn sdml_parser() -> Result<tree_sitter::Parser, tree_sitter::LanguageError> {
+        let mut parser = tree_sitter::Parser::new();
+        parser.set_language(&super::LANGUAGE.into())?;
+        Ok(parser)
+    }
+
     #[test]
     fn test_can_load_grammar() {
-        let mut parser = tree_sitter::Parser::new();
-        parser
-            .set_language(&super::language())
-            .expect("Error loading sdml language");
+        assert!(sdml_parser().is_ok());
     }
 
     const MANIFEST_PATH: &str = env!("CARGO_MANIFEST_DIR");
@@ -59,10 +65,7 @@ mod tests {
 
     #[test]
     fn test_parse_test_corpus() {
-        let mut parser = tree_sitter::Parser::new();
-        parser
-            .set_language(&super::language())
-            .expect("Error loading sdml language");
+        let mut parser = sdml_parser().expect("could not load grammar");
 
         for entry in fs::read_dir(format!("{MANIFEST_PATH}{TEST_PATH}")).expect("not a directory?")
         {
